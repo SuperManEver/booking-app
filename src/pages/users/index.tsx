2e1a45a9e64fb9bad134e8e70a4d7ecd9396387d
Nik Luparev <nikita.luparev@gmail.com>
2024-02-13 15:36:57 +0500

    split user details

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,9 +1,17 @@
+import { useState } from 'react';
+
+import { User } from 'types';
+
 import UsersList from 'components/users-list';
+import UserDetails from 'components/user-details';
 
 function UsersPage() {
+  const [user, setUser] = useState<User | null>(null);
+
   return (
     <main className="users-page">
-      <UsersList />
+      <UsersList user={user} setUser={setUser} />
+      <UserDetails user={user} />
     </main>
   );
 }
